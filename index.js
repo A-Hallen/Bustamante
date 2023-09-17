ce18dbcf71739e1bb746d7080c5bac5f469320a7
hallen <hallen412120@gmail.com>
2023-09-17 15:54:39 -0400

    just a commit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-// your code
+const buildpath = path.join(__dirname, "client", "build");
+
+app.use(express.static(buildpath));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(buildpath, "index.html"));
+});
+
+app.get("/proveedores", (req, res) => {
+  res.sendFile(path.join(buildpath, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
